77fa79765c4053325ec2ed4777deb4f9eb5010c0
rgard630 <rgard630@gmail.com>
2023-11-13 04:02:51 -0500

    finishedProject

---
+++ README.md
@@ -0,0 +1,31 @@
+# editext
+
+## Description
+A jate text editor
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+run npm run start, navigate to local 3000 in browser
+
+## Usage
+run npm run start in terminal, navigate to local 3000 in browser
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+no contributions needed
+
+## Tests
+no testing was needed
+
+## Questions
+- GitHub: [undefined](https://github.com/undefined)
+- Email: rgard630@gmail.com
--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,3 +1,4 @@
+// editor.js
 // Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from './database';
 import { header } from './header';
@@ -26,17 +27,19 @@ export default class {
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      this.editor.setValue(data?.content || localData || header);
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor to IndexedDB when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
-      putDb(localStorage.getItem('content'));
+      const content = this.editor.getValue();
+      putDb({ content });
     });
   }
 }
+
